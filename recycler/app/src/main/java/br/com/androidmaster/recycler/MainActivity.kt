46314a0ddf825014c67877f936fd0fad12d64d3f
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-02-23 15:51:18 -0300

    Add RecyclerAdapter

--- recycler/app/src/main/java/br/com/androidmaster/recycler/MainActivity.kt
+++ recycler/app/src/main/java/br/com/androidmaster/recycler/MainActivity.kt
@@ -2,10 +2,27 @@ package br.com.androidmaster.recycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.androidmaster.recycler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var messages = mutableListOf<Message>()
+    private var adapter = MessageAdapter(messages, this::onMessageItemClick)
+    lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun initRecyclerView(){
+        binding.rvMessages.adapter = adapter
+    }
+
+    private fun onMessageItemClick(message: Message){
+
     }
 }
--- recycler/app/src/main/java/br/com/androidmaster/recycler/MessageAdapter.kt
+++ recycler/app/src/main/java/br/com/androidmaster/recycler/MessageAdapter.kt
@@ -2,26 +2,37 @@ package br.com.androidmaster.recycler
 
 import android.view.View
 import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.androidmaster.recycler.databinding.ItemMessageBinding
 
 class MessageAdapter(
-    
-): RecyclerView.Adapter<MessageAdapter.VH>() {
+        val messages: List<Message>,
+        val callback: (Message) -> Unit):
+        RecyclerView.Adapter<MessageAdapter.VH>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
-        TODO("Not yet implemented")
+        val binding = ItemMessageBinding.inflate(LayoutInflater.from(parent.context))
+        val vh = VH(binding);
+        vh.itemView.setOnClickListener {
+            val message = messages[vh.adapterPosition]
+            callback(message)
+        }
+        return vh
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        TODO("Not yet implemented")
+        val (title, message) = messages[position]
+        holder.txtTitle.text = title
+        holder.txtMessage.text = message
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = messages.size
 
-    class VH(itemView: View): RecyclerView.ViewHolder(itemView){
-
+    class VH(binding: ItemMessageBinding): RecyclerView.ViewHolder(binding.root){
+        val txtTitle = binding.txtTitle
+        val txtMessage = binding.txtMessage
     }
 }
