cee2468905655b2d2165f808706618849c6ba52b
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-02-26 20:50:09 -0300

    Implement MessagesAdapter in the RecyclerView

--- recycler/app/src/main/java/br/com/androidmaster/recycler/MainActivity.kt
+++ recycler/app/src/main/java/br/com/androidmaster/recycler/MainActivity.kt
@@ -2,6 +2,9 @@ package br.com.androidmaster.recycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.androidmaster.recycler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +19,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        initRecyclerView()
+        binding.fabAdd.setOnClickListener {
+            addMessage()
+        }
+    }
+
+    private fun addMessage() {
+        val message = Message(
+                binding.edtTitle.text.toString(),
+                binding.edtMessage.text.toString()
+        )
+        messages.add(message)
+        adapter.notifyItemInserted(messages.lastIndex)
+        binding.edtTitle.text?.clear()
+        binding.edtMessage.text?.clear()
+        binding.edtTitle.requestFocus()
     }
 
     private fun initRecyclerView(){
         binding.rvMessages.adapter = adapter
+        val layoutManager = GridLayoutManager(this, 2)
+        binding.rvMessages.layoutManager = layoutManager
     }
 
     private fun onMessageItemClick(message: Message){
-
+        Toast.makeText(this@MainActivity,
+                getString(R.string.added, message.title),
+                Toast.LENGTH_SHORT).show()
     }
 }
