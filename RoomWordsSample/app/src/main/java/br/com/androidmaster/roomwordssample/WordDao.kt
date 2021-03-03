714559fe0db2f39a4cea84b52809cc1960c103d9
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-03 19:51:44 -0300

    Add returning LiveData

--- RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/WordDao.kt
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/WordDao.kt
@@ -1,5 +1,6 @@
 package br.com.androidmaster.roomwordssample
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -11,7 +12,7 @@ interface WordDao {
     fun insert(word: Word)
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getAllWords(): List<Word>
+    fun getAllWords(): LiveData<List<Word>>
 
     @Query("DELETE FROM word_table")
     fun deleteAll()
