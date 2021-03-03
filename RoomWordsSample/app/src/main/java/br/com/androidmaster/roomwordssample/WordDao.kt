b50a792bdda381e864508a0d4a07ad2e99f15162
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-03 18:57:48 -0300

    Create the DAO

---
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/WordDao.kt
@@ -0,0 +1,18 @@
+package br.com.androidmaster.roomwordssample
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface WordDao {
+
+    @Insert
+    fun insert(word: Word)
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAllWords(): List<Word>
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+}
