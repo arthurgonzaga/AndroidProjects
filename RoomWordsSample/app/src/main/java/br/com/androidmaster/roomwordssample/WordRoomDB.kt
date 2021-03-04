dd5f9e14f2f4a3b177a002c9d5ce2e5679a14d07
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-03 21:25:23 -0300

    Add a Room database

---
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/WordRoomDB.kt
@@ -0,0 +1,29 @@
+package br.com.androidmaster.roomwordssample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+abstract class WordRoomDB: RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object{
+        private var INSTANCE: WordRoomDB? = null
+
+        fun getInstance(context: Context): WordRoomDB{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    instance = Room.databaseBuilder(context,
+                        WordRoomDB::class.java,
+                        "word_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+                return instance
+            }
+        }
+    }
+}
