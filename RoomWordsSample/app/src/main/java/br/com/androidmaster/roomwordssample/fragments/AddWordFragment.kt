78aa2a1a3acb638291c97c7c7f71ac763a7a0080
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-06 12:36:35 -0300

    Implement viewModel DI and LifecycleObserver

--- RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/fragments/AddWordFragment.kt
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/fragments/AddWordFragment.kt
@@ -5,26 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import br.com.androidmaster.roomwordssample.data.model.Word
 import br.com.androidmaster.roomwordssample.viewmodels.WordViewModel
 import br.com.androidmaster.roomwordssample.databinding.FragmentAddWordBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class AddWordFragment : Fragment() {
+class AddWordFragment : Fragment(), LifecycleObserver{
 
     lateinit var binding: FragmentAddWordBinding
 
-    private lateinit var viewModel: WordViewModel
+    private val viewModel: WordViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentAddWordBinding.inflate(inflater)
+        lifecycle.addObserver(this)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate(){
+
+        binding.save.setOnClickListener {
+
+            val word = binding.edtWord.text.toString()
+
+            viewModel.insert(Word(word))
+        }
+
+        lifecycle.removeObserver(this)
     }
 }
