074a335cfc3ae54d9ff7926bc26c9dd71d712e2c
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-06 11:28:18 -0300

    Bug fix and Start koin

--- RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/applications/WordsApplication.kt
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/applications/WordsApplication.kt
@@ -3,12 +3,20 @@ package br.com.androidmaster.roomwordssample.applications
 import android.app.Application
 import br.com.androidmaster.roomwordssample.data.local.word.WordRepository
 import br.com.androidmaster.roomwordssample.data.local.word.WordRoomDB
+import br.com.androidmaster.roomwordssample.di.modules.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class WordsApplication(): Application() {
-    lateinit var database: WordRoomDB
-    val repository by lazy { WordRepository(database.wordDao()) }
+
     override fun onCreate() {
         super.onCreate()
-        database = WordRoomDB.getInstance(this)
+
+        startKoin {
+            androidLogger()
+            androidContext(this@WordsApplication)
+            modules(appModule)
+        }
     }
 }
