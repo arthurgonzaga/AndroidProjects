3ebf9cc2e7dc9c0d8b6bdad617776f8a38a5fb86
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-06 11:29:58 -0300

    Simplify database

--- RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/data/local/word/WordRoomDB.kt
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/data/local/word/WordRoomDB.kt
@@ -6,25 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import br.com.androidmaster.roomwordssample.data.model.Word
 
-@Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
+@Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDB: RoomDatabase() {
     abstract fun wordDao(): WordDao
-
-    companion object{
-        private var INSTANCE: WordRoomDB? = null
-
-        fun getInstance(context: Context): WordRoomDB {
-            synchronized(this){
-                var instance = INSTANCE
-                if(instance == null){
-                    instance = Room.databaseBuilder(context,
-                        WordRoomDB::class.java,
-                        "word_database")
-                        .fallbackToDestructiveMigration()
-                        .build()
-                }
-                return instance
-            }
-        }
-    }
 }
