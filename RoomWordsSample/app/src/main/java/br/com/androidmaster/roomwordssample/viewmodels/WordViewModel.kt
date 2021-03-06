e8f2546511eb38c238dd664d74ed072d6a0fbd98
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-06 12:11:38 -0300

    Change allWords to public

--- RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/viewmodels/WordViewModel.kt
+++ RoomWordsSample/app/src/main/java/br/com/androidmaster/roomwordssample/viewmodels/WordViewModel.kt
@@ -7,28 +7,9 @@ import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository): ViewModel() {
 
-    private val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
 
     fun insert(word: Word) = viewModelScope.launch {
         repository.insert(word)
-    }
-
-    fun isEmpty(): Boolean{
-        if(allWords.value != null){
-            if(allWords.value!!.size >= 0) return false
-        }
-        return true
-    }
-
-}
-
-class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return WordViewModel(repository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
