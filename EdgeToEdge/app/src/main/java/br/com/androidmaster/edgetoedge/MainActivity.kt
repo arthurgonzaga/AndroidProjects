f3a595b8a1042bebc8aed362230e7f2309540e36
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-21 14:01:41 -0300

    Setup Project

--- EdgeToEdge/app/src/main/java/br/com/androidmaster/edgetoedge/MainActivity.kt
+++ EdgeToEdge/app/src/main/java/br/com/androidmaster/edgetoedge/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpRecyclerView()
     }
 
     fun setUpRecyclerView(){
