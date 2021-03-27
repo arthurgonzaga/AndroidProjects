987734979fdb0605ef3c72355ea1e73b4a66d938
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-03-27 17:19:50 -0300

    Implement show notifications

--- Notification/app/src/main/java/br/com/androidmaster/notifications/MainActivity.kt
+++ Notification/app/src/main/java/br/com/androidmaster/notifications/MainActivity.kt
@@ -1,15 +1,22 @@
 package br.com.androidmaster.notifications
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var button1: Button
     lateinit var button2: Button
-    lateinit var button3: Button
+    lateinit var buttonDelete: Button
+
+    // The IDs is incremented when new notifications appear
+    var notificationId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +24,35 @@ class MainActivity : AppCompatActivity() {
 
         button1 = findViewById(R.id.button1)
         button2 = findViewById(R.id.button2)
-        button3 = findViewById(R.id.button3)
+        buttonDelete = findViewById(R.id.button3)
     }
 
+    /**
+     * This is just an example of a notification
+     */
     fun createNotification1(view: View){
+        val CHANNEL_ID = getString(R.string.CHANNEL_1_ID)
 
+        // Define the activity
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK }
+        val pendingIntent = PendingIntent.getActivity(this, 0,intent,0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_cake_24)
+            .setContentTitle("Title test")
+            .setContentText("This is the content of the notification")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)){
+            // Remove the last notification
+            cancel(notificationId)
+            notificationId++
+
+            // Show the notification
+            notify(notificationId, builder.build())
+        }
     }
+
 }
