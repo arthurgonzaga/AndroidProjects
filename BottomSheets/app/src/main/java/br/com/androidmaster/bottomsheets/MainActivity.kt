d0a58d50e65f23a3e6dc5c4d8a58448d3b4c5fac
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-05-02 20:00:32 -0300

    Implement full height BottomSheetDialog

--- BottomSheets/app/src/main/java/br/com/androidmaster/bottomsheets/MainActivity.kt
+++ BottomSheets/app/src/main/java/br/com/androidmaster/bottomsheets/MainActivity.kt
@@ -1,20 +1,27 @@
 package br.com.androidmaster.bottomsheets
 
+import android.R
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import br.com.androidmaster.bottomsheets.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    fun openBottomSheet(view: View) {
+    fun openBottomSheetDialog(view: View) {
         supportFragmentManager.let {
-            TestBottomSheetFragment.newInstance().apply {
+            TestBottomSheetFragment.newInstance(this).apply {
                 show(it, tag)
             }
         }
--- BottomSheets/app/src/main/java/br/com/androidmaster/bottomsheets/TestBottomSheetFragment.kt
+++ BottomSheets/app/src/main/java/br/com/androidmaster/bottomsheets/TestBottomSheetFragment.kt
@@ -1,35 +1,73 @@
 package br.com.androidmaster.bottomsheets
 
+import android.app.Activity
+import android.app.Dialog
+import android.content.DialogInterface.OnShowListener
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.DisplayMetrics
+import android.view.*
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
-import br.com.androidmaster.bottomsheets.databinding.BottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlin.properties.Delegates
 
-class TestBottomSheetFragment : BottomSheetDialogFragment() {
 
-    lateinit var binding: BottomSheetBinding
+class TestBottomSheetFragment(val activity: Activity) : BottomSheetDialogFragment() {
+
+    var peekHeight by Delegates.notNull<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = BottomSheetBinding.inflate(inflater)
-        return binding.root
+        peekHeight = resources.getDimensionPixelSize(R.dimen.peekHeight_small)
+        return layoutInflater.inflate(R.layout.bottom_sheet, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener {
+        view.findViewById<Button>(R.id.button).setOnClickListener {
             Toast.makeText(requireContext(), "TESTE", Toast.LENGTH_SHORT).show()
         }
     }
 
-    companion object{
-        fun newInstance() = TestBottomSheetFragment()
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog: Dialog = super.onCreateDialog(savedInstanceState)
+
+        dialog.setOnShowListener { dialogInterface ->
+
+            val bottomSheetDialog = dialogInterface as BottomSheetDialog
+            setupFullHeight(bottomSheetDialog)
+        }
+        return dialog
     }
+
+
+    private fun setupFullHeight(bottomSheetDialog: BottomSheetDialog) {
+        val bottomSheet = bottomSheetDialog.findViewById<View>(R.id.design_bottom_sheet) as FrameLayout
+        val layoutParams = bottomSheet.layoutParams
+        val windowHeight = getWindowHeight()
+        if (layoutParams != null) {
+            layoutParams.height = windowHeight
+        }
+        bottomSheet.layoutParams = layoutParams
+        bottomSheetDialog.behavior.peekHeight = peekHeight
+    }
+
+    private fun getWindowHeight(): Int {
+        // Calculate window height for fullscreen use
+        val displayMetrics = DisplayMetrics()
+        (context as Activity?)!!.windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.heightPixels
+    }
+
+
+    companion object{
+        fun newInstance(activity: Activity) = TestBottomSheetFragment(activity)
+    }
+
 }
