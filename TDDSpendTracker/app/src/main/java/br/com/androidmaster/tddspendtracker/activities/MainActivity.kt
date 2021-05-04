f6731a4f15bdb155386611c0df5ce51dc832b157
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-05-04 00:47:03 -0300

    Initial Commit

---
+++ TDDSpendTracker/app/src/main/java/br/com/androidmaster/tddspendtracker/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.androidmaster.tddspendtracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.androidmaster.tddspendtracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
