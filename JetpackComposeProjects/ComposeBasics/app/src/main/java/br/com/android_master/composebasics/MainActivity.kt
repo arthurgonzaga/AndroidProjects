1fd21dde466cb7079bb75f93c74663dab4308323
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-05-23 11:32:44 -0300

    Implement a simple list

--- JetpackComposeProjects/ComposeBasics/app/src/main/java/br/com/android_master/composebasics/MainActivity.kt
+++ JetpackComposeProjects/ComposeBasics/app/src/main/java/br/com/android_master/composebasics/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -24,12 +27,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp{
-                NewsStory()
+                Column{
+                    NewsStory()
+                    ListOfContent()
+                }
             }
         }
     }
 }
 
+
+@Composable
+fun ListOfContent(names: List<String> = listOf("alo", "teste","top")) {
+    Column{
+        for(name in names){
+            Text(name,
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp, horizontal = 24.dp))
+            Divider(color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 24.dp))
+        }
+    }
+}
 
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
@@ -48,7 +68,10 @@ fun NewsStory() {
         Image(
             painter = painterResource(R.drawable.header),
             contentDescription = null,
-            modifier = Modifier.clip(Shapes.medium).height(180.dp).fillMaxWidth(),
+            modifier = Modifier
+                .clip(Shapes.medium)
+                .height(180.dp)
+                .fillMaxWidth(),
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.height(16.dp))
@@ -63,11 +86,18 @@ fun NewsStory() {
         Spacer(Modifier.height(8.dp))
         Text("Davenport, California", style = MaterialTheme.typography.body2)
         Text("December 2018", style = MaterialTheme.typography.body2)
+    
+    
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    NewsStory()
+    MyApp{
+        Column{
+            NewsStory()
+            ListOfContent()
+        }
+    }
 }
