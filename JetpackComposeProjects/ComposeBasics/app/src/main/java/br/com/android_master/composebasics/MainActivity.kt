8e752579c1abad24a34d7b53b7c18b5f55e91a8b
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-05-23 12:16:28 -0300

    Implement a counter button with state

--- JetpackComposeProjects/ComposeBasics/app/src/main/java/br/com/android_master/composebasics/MainActivity.kt
+++ JetpackComposeProjects/ComposeBasics/app/src/main/java/br/com/android_master/composebasics/MainActivity.kt
@@ -1,16 +1,16 @@
 package br.com.android_master.composebasics
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -32,21 +32,6 @@ class MainActivity : ComponentActivity() {
                     ListOfContent()
                 }
             }
-        }
-    }
-}
-
-
-@Composable
-fun ListOfContent(names: List<String> = listOf("alo", "teste","top")) {
-    Column{
-        for(name in names){
-            Text(name,
-                Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp, horizontal = 24.dp))
-            Divider(color = Color.LightGray,
-                modifier = Modifier.padding(horizontal = 24.dp))
         }
     }
 }
@@ -88,6 +73,42 @@ fun NewsStory() {
         Text("December 2018", style = MaterialTheme.typography.body2)
     
     
+    }
+}
+
+
+@Composable
+fun ListOfContent(names: List<String> = listOf("alo", "teste","top")) {
+
+    val counterState = remember{ mutableStateOf(0) }
+
+    Column(Modifier.fillMaxHeight()){
+        Column(Modifier.weight(1f)){
+            for(name in names){
+                Text(name,
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp, horizontal = 24.dp))
+                Divider(color = Color.LightGray,
+                    modifier = Modifier.padding(horizontal = 24.dp))
+            }
+        }
+
+        Row(Modifier.fillMaxWidth(), Arrangement.Center){
+            CounterButton(
+                count = counterState.value,
+                onClick = { newValue ->
+                    counterState.value = newValue
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun CounterButton(count: Int, onClick: (newValue: Int)-> Unit) {
+    Button(modifier= Modifier.padding(24.dp), onClick = { onClick(count+1) }) {
+        Text("You've clicked in this button $count times")
     }
 }
 
