9bc2c95654f52a4835be923a72928d17e8413de9
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-05-26 00:24:45 -0300

    Implement TopAppBar

--- JetpackComposeProjects/ComposeLayouts/app/src/main/java/br/com/android_master/composelayouts/MainActivity.kt
+++ JetpackComposeProjects/ComposeLayouts/app/src/main/java/br/com/android_master/composelayouts/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -16,29 +19,38 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import br.com.android_master.composelayouts.ui.theme.ComposeLayoutsTheme
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             ComposeLayoutsTheme {
-                PhotographerCard()
+                ProvideWindowInsets {
+                    LayoutCodeLab()
+                }
             }
         }
     }
 }
 
 @Composable
-fun PhotographerCard() {
+fun PhotographerCard(modifier: Modifier) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .padding(16.dp)
+        modifier = modifier
+            .padding(18.dp)
             .clip(MaterialTheme.shapes.medium)
             .clickable { }
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(12.dp)
     ){
         Surface(
             modifier = Modifier.size(50.dp),
@@ -51,18 +63,47 @@ fun PhotographerCard() {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             // LocalContentAlpha is defining opacity level of its children
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+                Text("Thanks for going through the Layouts codelab", style = MaterialTheme.typography.body2)
             }
         }
     }
 
 }
 
+@Composable
+fun LayoutCodeLab() {
+
+    val systemUiController = rememberSystemUiController()
+    val darkColor = MaterialTheme.colors.isLight
+    SideEffect {
+        systemUiController.setStatusBarColor(Color.Transparent, darkIcons = darkColor)
+    }
+
+    Scaffold(
+        Modifier.statusBarsPadding(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("LayoutCodelab", color = MaterialTheme.colors.onBackground)
+                },
+                actions= {
+                         IconButton(onClick = {}) {
+                             Icon(Icons.Filled.Share, contentDescription = "Share")
+                         }
+                },
+                elevation = 0.dp,
+                backgroundColor = MaterialTheme.colors.background,
+            )
+        }
+    ){
+        PhotographerCard(Modifier.padding(paddingValues = it))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun PhotographerCardPreview() {
     ComposeLayoutsTheme {
-        PhotographerCard()
+        LayoutCodeLab()
     }
 }
