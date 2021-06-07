92b920e3b11fcea1e3422922dfda895a8e364d5b
arthurgonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-06-07 01:45:28 -0300

    Implement UserScreen

--- JetpackComposeProjects/LoginApp/app/src/main/java/br/com/android_master/loginapp/UserScreen.kt
+++ JetpackComposeProjects/LoginApp/app/src/main/java/br/com/android_master/loginapp/UserScreen.kt
@@ -1,9 +1,11 @@
 package br.com.android_master.loginapp
 
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
@@ -11,18 +13,41 @@ import com.google.firebase.auth.FirebaseUser
 
 @Composable
 fun UserScreen(
-    currentUser: FirebaseUser? = null,
+    currentUser: FirebaseUser,
     onLogoutButtonClick: ()-> Unit
 ) {
-    Row(
-        modifier= Modifier.fillMaxWidth().padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceAround
-    ){
-        Text("Você está logado, ${currentUser!!.displayName}!")
-        Button(
-            onClick= onLogoutButtonClick
+    var isEmailVerified by remember{ mutableStateOf(currentUser.isEmailVerified) }
+
+    if(isEmailVerified){
+        Row(
+            modifier= Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceAround
         ){
-            Text("Sair")
+            Text("Você está logado, ${currentUser.displayName}!")
+            Button(
+                onClick= onLogoutButtonClick
+            ){
+                Text("Sair")
+            }
+        }
+    }else{
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ){
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
+                Text("Verifique seu email...")
+                Spacer(Modifier.height(16.dp))
+                Button(
+                    onClick= onLogoutButtonClick
+                ){
+                    Text("Sair")
+                }
+            }
         }
     }
 }
